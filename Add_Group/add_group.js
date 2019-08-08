20c41790a0e856a8895915123ce2af7072231c50
Noa Strupinsky <noa.strupinsky@RBCCM-15s-MacBook-Air.local>
2019-08-08 15:31:54 -0400

    email api works through firebase and puts in the link and the join page works and appends to url

--- Add_Group/add_group.js
+++ Add_Group/add_group.js
@@ -1,32 +1,10 @@
 
-
-
-
-
 let submit_button = document.getElementById("submit");
 let add_another_email = document.getElementById('add_email');
 
 let email_list = [];
 let length_emails = 1;
 
-let name = document.getElementsByName('group_name')[0].value;
-let description = document.getElementsByName('description')[0].value;
-
-let url =
-
-
-add_another_email.onclick = function(ev) {
-  console.log(length_emails);
-  var para = document.createElement("input");
-  para.classList.add("emails");
-  para.setAttribute("type", "email")
-  var node = document.createTextNode("");
-  para.appendChild(node);
-  var element = document.getElementById("div1");
-  element.appendChild(para);
-  length_emails++;
-
-};
 
 
 submit_button.onclick = function(){
@@ -35,7 +13,19 @@ submit_button.onclick = function(){
     var emails = document.getElementsByClassName('emails')[i].value;
     email_list.push(emails);
     console.log(email_list);
+};
+
 }
-  sgMail.send(msg);
-  console.log("sent");
+add_another_email.onclick = function(ev) {
+  length_emails++;
+  console.log(length_emails);
+  var para = document.createElement("input");
+  para.classList.add("emails");
+  para.setAttribute("type", "email");
+  var node = document.createTextNode("");
+  para.appendChild(node);
+  var element = document.getElementById("div1");
+  element.appendChild(para);
+
+
 };
---
+++ join_group/join_group.js
@@ -0,0 +1,65 @@
+// alert("hey");
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+var number = getUrlVars()["group_name"];
+
+console.log(number);
+document.getElementById("variable").innerHTML=number;
+
+
+let email_user;
+let insta_user;
+let snap_user;
+let link_user;
+let twitter_user;
+
+var email_check = document.getElementById("email_check");
+var insta_check = document.getElementById('insta_check')
+var snap_check = document.getElementById('snap_check');
+var link_check = document.getElementById('link_check');
+var titter_check = document.getElementById('twitter_check');
+
+var button = document.getElementById('test');
+
+let link = "/Users/noa.strupinsky/blue_pages/public/home.html?";
+
+
+
+button.onclick = function(){
+
+  if (email_check.checked == true) {
+    email_user = document.getElementById('email_user').value;
+    console.log(email_user);
+    link = link + "email_user=" + email_user + "&";
+  }
+
+  if (insta_check.checked == true) {
+    insta_user = document.getElementById('insta_user').value;
+      console.log(insta_user);
+      link = link + "insta_user=" + insta_user + "&";
+  }
+  if (snap_check.checked == true) {
+    snap_user = document.getElementById('snap_user').value;
+      console.log(snap_user);
+      link = link + "snap_user=" + snap_user + "&";
+  }
+  if (link_check.checked == true) {
+    link_user = document.getElementById('link_user').value;
+      console.log(link_user);
+      link = link + "link_user=" + link_user + "&";
+  }
+  if (twitter_check.checked == true) {
+    twitter_user = document.getElementById('twitter_user').value;
+      console.log(twitter_user);
+      link = link + "twitter_user=" + twitter_user;
+  }
+
+
+  location.href = link;
+}
