5e992fe1a7ce31513a024ea004d968fb06139e94
Noa Strupinsky <noa.strupinsky@RBCCM-15s-MacBook-Air.local>
2019-08-08 10:50:54 -0400

    email apt works through firebase

--- Add_Group/add_group.js
+++ Add_Group/add_group.js
@@ -1,39 +1,41 @@
 
 
+
+
+
 let submit_button = document.getElementById("submit");
-
-submit_button.onclick = function(){
-  for (var i = 0; i < 100; i++ ){
-  var emails= document.getElementsByName('email')[0].value;
-  console.log(group_name);
-}
-}
-
-
-
-
-
-
-
 let add_another_email = document.getElementById('add_email');
 
+let email_list = [];
+let length_emails = 1;
+
+let name = document.getElementsByName('group_name')[0].value;
+let description = document.getElementsByName('description')[0].value;
+
+let url =
+
+
 add_another_email.onclick = function(ev) {
+  console.log(length_emails);
   var para = document.createElement("input");
+  para.classList.add("emails");
+  para.setAttribute("type", "email")
   var node = document.createTextNode("");
   para.appendChild(node);
-  var element = document.getElementById("emails");
+  var element = document.getElementById("div1");
   element.appendChild(para);
+  length_emails++;
+
 };
 
-/*
-var email_list = []
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: email_list,
-  from: 'www.follow.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+
+submit_button.onclick = function(){
+
+  for ( let i = 0; i < length_emails; i++ ){
+    var emails = document.getElementsByClassName('emails')[i].value;
+    email_list.push(emails);
+    console.log(email_list);
+}
+  sgMail.send(msg);
+  console.log("sent");
 };
-sgMail.send(msg); */
