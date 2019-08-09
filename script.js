2d8f9c558dd1e503869a8161718959f206cd745a
Erin Haddad <Erin@RBCCM-13s-MacBook-Air.local>
2019-08-09 09:55:49 -0400

    home page drop down works

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+ function getUrlVars() {
+      var vars = {};
+      var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+           vars[key] = value;
+      });
+      return vars;
+ }
+
+ var name = getUrlVars()["group_name"];
+ document.getElementById("variable").innerHTML=name;
+
+console.log(name);
