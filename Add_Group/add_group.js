6aea6758c0428c96de3481637bf5fad01bf4f19c
Noa Strupinsky <noa.strupinsky@RBCCM-15s-MacBook-Air.local>
2019-08-15 14:17:18 -0400

    email worsk

--- Add_Group/add_group.js
+++ Add_Group/add_group.js
@@ -10,15 +10,16 @@ let length_emails = 1;
 
 submit_button.onclick = function(){
 let group_name = document.getElementById("group_name").value;
+let email = document.getElementById("emails").value;
 
 var db = firebase.firestore();
 
-db.collection("groups").doc(group_name).set({
-  
-})
+db.collection("groups").doc(group_name).set({})
 .then(function() {
     console.log("Document successfully written!");
-    location.href = "https://us-central1-waldo-7996a.cloudfunctions.net/email"
+    let link = "https://us-central1-waldo-7996a.cloudfunctions.net/email?" + "group_name=" + group_name +"&email=" + email;
+    console.log(link);
+    window.location.href = link
 })
 .catch(function(error) {
     console.error("Error writing document: ", error);
